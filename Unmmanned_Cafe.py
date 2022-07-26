e76e2d9a8c9223c5d8148a90dba1ab8a28d97c69
yeonggwangchoi <dudrhkd7811@naver.com>
2022-07-26 22:21:23 +0900

    Update: 기능 함수로 구현

--- Unmmanned_Cafe.py
+++ Unmmanned_Cafe.py
@@ -1,32 +1,59 @@
-print("Glory coffee에 오신걸 환영합니다~")
+import sys
 
-while True:
-    #메뉴 보여주기
+print("Glory coffee에 오신걸 환영합니다.")
+
+def show_menu():
     print("==<< 메뉴 >>==")
     print("1. 아메리카노")
     print("2. 카페라떼")
     print("3. 에스프레소")
     print("4. 녹차")
     print("5. 망고쥬스")
-    
-    #주문 받기
+    return
+def get_order():
     order = input("무엇을 주문하시겠어요?(q.종료) : ")
     if order == "q":
         print("주문이 취소 되었습니다!")
-        break;
+        sys.exit(0)
     print(order, "주문하셨습니다.")
-    
+    return order
+def make_americano():
+    print("아메리카노를 만들고 있습니다~")
+    return
+def make_cafelatte():
+    print("카페라떼를 만들고 있습니다~")
+    return
+def make_espresso():
+    print("에스프레소를 만들고 있습니다~")
+    return
+def make_greentee():
+    print("녹차를 만들고 있습니다~")
+    return
+def make_mangojuice():
+    print("망고쥬스를 만들고 있습니다~")
+    return
+
+def process_order(order):
     if order == "1":
-        print("아메리카노를 만들고 있습니다~")
+        make_americano()
     elif order == "2":
-        print("카페라떼를 만들고 있습니다~")
+        make_cafelatte()
     elif order == "3":
-        print("에스프레소를 만들고 있습니다~")
+       make_espresso()
     elif order == "4":
-        print("녹차를 만들고 있습니다~")
+       make_greentee()
     elif order == "5":
-        print("망고쥬스를 만들고 있습니다~")
+       make_mangojuice()
     else:
         print("다시 주문해주세요~")
-        
-        test!!
+    return
+
+while True:
+    #메뉴 보여주기
+    show_menu()
+    
+    #주문 받기
+    order=get_order()
+    
+    #주문 처리하기
+    process_order(order)
