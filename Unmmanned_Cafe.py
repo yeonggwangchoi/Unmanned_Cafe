2cb08e058ad9652db0f7fd085ee1a22f452e312b
yeonggwangchoi <dudrhkd7811@naver.com>
2022-07-31 08:50:21 +0900

    Update: 메뉴 일반화, 메뉴 리스트화

--- Unmmanned_Cafe.py
+++ Unmmanned_Cafe.py
@@ -1,42 +1,46 @@
+from distutils.command.build_scripts import first_line_re
 import sys
 
 print("Glory coffee에 오신걸 환영합니다.")
-print("메뉴판을 보여드릴까요?")
-menu = input("yes 아니면 no를 입력하세요> ")
-if menu == "yes":
-    print("")
-else:
-    print("")
+
+
 
 def show_menu():
     print("==<< 메뉴 >>==")
-    print("1. 아메리카노")
-    print("2. 카페라떼")
-    print("3. 에스프레소")
-    print("4. 녹차")
-    print("5. 망고쥬스")
+    for item in menu:
+        print(item)
     return
+
+menu = [
+    "1. 아메리카노",
+    "2. 카페라떼",
+    "3. 에스프레소",
+    "4. 녹차",
+    "5. 망고쥬스"
+]
+
 def get_order():
     order = input("무엇을 주문하시겠어요?(q.종료) : ")
     if order == "q":
-        print("주문이 취소 되었습니다!")
+        print("주문이 취소 되었습니다.")
         sys.exit(0)
-    print(order, "주문하셨습니다.")
+    print(order, "번을 주문하셨습니다.")
     return order
+
 def make_americano():
-    print("아메리카노를 만들고 있습니다~")
+    print("아메리카노를 만들고 있습니다.")
     return
 def make_cafelatte():
-    print("카페라떼를 만들고 있습니다~")
+    print("카페라떼를 만들고 있습니다.")
     return
 def make_espresso():
-    print("에스프레소를 만들고 있습니다~")
+    print("에스프레소를 만들고 있습니다.")
     return
 def make_greentee():
-    print("녹차를 만들고 있습니다~")
+    print("녹차를 만들고 있습니다.")
     return
 def make_mangojuice():
-    print("망고쥬스를 만들고 있습니다~")
+    print("망고쥬스를 만들고 있습니다.")
     return
 
 def process_order(order):
@@ -55,14 +59,21 @@ def process_order(order):
     return
 
 while True:
-    #메뉴 보여주기
-    show_menu()
+    print("주문 하시겠습니까?")
+    first = input("yes 아니면 no를 입력하세요> ")
+    if first == "yes":
+        #메뉴 보여주기
+        print("메뉴판을 보여드겠습니다.")
+        show_menu()
+        #주문 받기
+        order=get_order()
     
-    #주문 받기
-    order=get_order()
+        #주문 처리하기
+        process_order(order)
+    else:
+        print("주문을 취소합니다.")
+        sys.exit(0)
+   
     
-    #주문 처리하기
-    process_order(order)
+
     
-    #영업 종료 기능 추가 할 것
-    #종료 기능 구현
